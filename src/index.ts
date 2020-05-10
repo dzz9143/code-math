3879880abd79cc03bc2f31c6d8b234743d592fbc
zhizhuoding <zhizhuoding@qq.com>
2020-05-10 22:13:30 +0800

    simple hello program

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 function main(win: Window, doc: Document): void {
-    console.log('win:', win);
-    console.log('doc:', doc);
+    // hello
+    console.log('hello code math');
+
+    const canvas = doc.getElementById('canvas') as HTMLCanvasElement;
+    canvas.width = win.innerWidth;
+    canvas.height = win.innerHeight;
+    const width = win.innerWidth;
+    const height = win.innerHeight;
+
+    console.log('init canvas with w:', width, 'h:', height);
+
+    const context = canvas.getContext('2d');
+
+    for (let i = 0; i < 100; i++) {
+        context.moveTo(Math.random() * width, Math.random() * height);
+        context.lineTo(Math.random() * width, Math.random() * height);
+        context.stroke();
+    }
 }
 
-main(window, document);
+window.onload = function onload(): void {
+    main(window, document);
+};
