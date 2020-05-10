2e045de7688d8e7f0dc86039a29eee0267516690
zhizhuoding <zhizhuoding@qq.com>
2020-05-10 20:23:39 +0800

    setup webpack env

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+/*eslint @typescript-eslint/no-var-requires: off */
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: './src/index.ts',
+    },
+    output: {
+        filename: '[name].[hash:8].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Code Math',
+        }),
+    ],
+};
