0767054860964a1a996feb690dbb2793f1c9807b
Zhizhuo Ding <zhizhuoding@qq.com>
2020-10-31 01:53:25 +0800

    create grid class and group pathfinding logic in grid class

--- src/applications/game/main.ts
+++ src/applications/game/main.ts
@@ -1,14 +1,277 @@
-import { Particle } from '../../particle';
-import { keyCodes } from '../../constant';
-import { Vector } from '../../vector';
-import { degToR, randRange } from '../../utility';
-import { Point } from '../../types';
 import PQ from 'priorityqueuejs';
 
-type PointWithCost = {
-    id: number;
-    cost: number;
+import { keyCodes } from '../../constant';
+import { clamp, randRange } from '../../utility';
+
+type Tile = {
+    c: number;
+    r: number;
+    cost?: number;
+    id?: number;
 };
+
+class Grid {
+    public row: number;
+    public col: number;
+    public tileSize: number;
+
+    // top left corner position
+    public x: number;
+    public y: number;
+
+    public top: number;
+    public bottom: number;
+    public left: number;
+    public right: number;
+
+    constructor(row: number, col: number, tileSize: number, x = 0, y = 0) {
+        this.row = row;
+        this.col = col;
+        this.tileSize = tileSize;
+
+        this.x = x;
+        this.y = y;
+
+        this.left = this.x;
+        this.right = this.x + this.col * this.tileSize;
+        this.top = this.y;
+        this.bottom = this.y + this.row * this.tileSize;
+    }
+
+    public render(ctx: CanvasRenderingContext2D): void {
+        ctx.save();
+        ctx.strokeStyle = 'rgb(200, 200, 200)';
+        ctx.beginPath();
+
+        for (let i = 0; i < this.row; i++) {
+            const y = i * this.tileSize;
+            ctx.moveTo(this.left, y);
+            ctx.lineTo(this.right, y);
+        }
+
+        for (let i = 0; i < this.col; i++) {
+            const x = i * this.tileSize;
+            ctx.moveTo(x, this.top);
+            ctx.lineTo(x, this.bottom);
+        }
+        ctx.stroke();
+        ctx.restore();
+    }
+
+    private createTile(row: number, col: number): Tile {
+        return {
+            c: col,
+            r: row,
+            cost: 0,
+            id: row * this.col + col,
+        };
+    }
+
+    private getTileFromWorldPos(worldX: number, worldY: number): Tile {
+        const x = clamp(worldX, this.left, this.right);
+        const y = clamp(worldY, this.top, this.bottom);
+        const c = Math.floor(x / this.tileSize);
+        const r = Math.floor(y / this.tileSize);
+
+        return this.createTile(r, c);
+    }
+
+    private getNeighborTiles(cur: Tile): Tile[] {
+        const neighborTiles: Tile[] = [];
+        if (cur.r - 1 >= 0) {
+            neighborTiles.push(this.createTile(cur.r - 1, cur.c));
+        }
+
+        if (cur.c + 1 < this.col) {
+            neighborTiles.push(this.createTile(cur.r, cur.c + 1));
+        }
+
+        if (cur.r + 1 < this.row) {
+            neighborTiles.push(this.createTile(cur.r + 1, cur.c));
+        }
+
+        if (cur.c - 1 >= 0) {
+            neighborTiles.push(this.createTile(cur.r, cur.c - 1));
+        }
+        return neighborTiles;
+    }
+
+    private heuristic(tileA: Tile, tileB: Tile): number {
+        const dx = tileA.c - tileB.c;
+        const dy = tileA.r - tileB.r;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    public findPath(fromX: number, fromY: number, toX: number, toY: number): Tile[] {
+        const fromTile = this.getTileFromWorldPos(fromX, fromY);
+        const toTile = this.getTileFromWorldPos(toX, toY);
+
+        if (fromTile.id === toTile.id) {
+            return [];
+        }
+
+        const frontier = new PQ((a: Tile, b: Tile): number => b.cost - a.cost);
+
+        frontier.enq(fromTile);
+        const comeFrom = new Map<number, Tile>();
+        const costSoFar = {
+            [fromTile.id]: 0,
+        };
+
+        let found = false;
+        while (frontier.size() > 0) {
+            const current = frontier.deq();
+            if (current.id === toTile.id) {
+                // found it
+                found = true;
+                break;
+            }
+            const neighborTiles = this.getNeighborTiles(current);
+            for (let i = 0; i < neighborTiles.length; i++) {
+                const neighborTile = neighborTiles[i];
+                const newCost = costSoFar[current.id] + 1;
+                if (!costSoFar[neighborTile.id] || newCost < costSoFar[neighborTile.id]) {
+                    costSoFar[neighborTile.id] = newCost;
+                    const priority = newCost + this.heuristic(toTile, neighborTile);
+                    neighborTile.cost = priority;
+                    frontier.enq(neighborTile);
+                    comeFrom.set(neighborTile.id, current);
+                }
+            }
+        }
+
+        if (!found) {
+            return [];
+        }
+        const tiles = [];
+        let id = toTile.id;
+        while (comeFrom.get(id) !== fromTile) {
+            const nextTile = comeFrom.get(id);
+            tiles.push(nextTile);
+            id = nextTile.id;
+        }
+
+        return tiles;
+    }
+}
+
+class Square {
+    public x: number;
+    public y: number;
+    public w: number;
+    public h: number;
+    public color: string;
+    constructor(x: number, y: number, w: number, h: number) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+
+        this.color = 'rgb(255, 255, 255)';
+    }
+
+    render(ctx: CanvasRenderingContext2D): void {
+        ctx.save();
+
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+
+        ctx.restore();
+    }
+}
+
+class UserInput {
+    private keyDownMap: any;
+
+    constructor(doc: Document) {
+        this.keyDownMap = {};
+        doc.addEventListener('keydown', this.keyDownEventListener);
+    }
+
+    private keyDownEventListener = (ev: KeyboardEvent): void => {
+        const keyCode = ev.keyCode;
+        this.keyDownMap[keyCode] = true;
+    };
+
+    public clear(): void {
+        this.keyDownMap = {};
+    }
+
+    public isKeyDown(code: number): boolean {
+        return this.keyDownMap[code];
+    }
+}
+
+class Player extends Square {
+    private grid: Grid;
+    private speed: number;
+
+    constructor(grid: Grid) {
+        super(grid.x, grid.y, 30, 30);
+        this.grid = grid;
+        this.speed = 15;
+        this.color = 'rgb(200, 0, 0)';
+    }
+
+    public update(userInput: UserInput): void {
+        if (userInput.isKeyDown(keyCodes.KEY_D)) {
+            this.x += this.speed;
+        }
+
+        if (userInput.isKeyDown(keyCodes.KEY_A)) {
+            this.x -= this.speed;
+        }
+
+        if (userInput.isKeyDown(keyCodes.KEY_W)) {
+            this.y -= this.speed;
+        }
+
+        if (userInput.isKeyDown(keyCodes.KEY_S)) {
+            this.y += this.speed;
+        }
+
+        this.x = clamp(this.x, this.grid.left, this.grid.right - this.w);
+        this.y = clamp(this.y, this.grid.top, this.grid.bottom - this.h);
+    }
+}
+
+class Enemy extends Square {
+    private grid: Grid;
+    private player: Player;
+    private tiles: Tile[];
+    constructor(grid: Grid, player: Player) {
+        super(
+            grid.x + randRange(grid.left, grid.right),
+            grid.y + randRange(grid.top, grid.bottom),
+            30,
+            30,
+        );
+        this.grid = grid;
+        this.player = player;
+        this.color = 'rgb(0, 200, 0)';
+        this.tiles = [];
+    }
+
+    update(): void {
+        this.tiles = this.grid.findPath(this.x, this.y, this.player.x, this.player.y);
+    }
+
+    render(ctx: CanvasRenderingContext2D): void {
+        super.render(ctx);
+
+        ctx.save();
+        ctx.fillStyle = 'rgb(200, 200, 200)';
+        this.tiles.forEach((t) => {
+            ctx.fillRect(
+                this.grid.x + t.c * this.grid.tileSize,
+                this.grid.y + t.r * this.grid.tileSize,
+                50,
+                50,
+            );
+        });
+        ctx.restore();
+    }
+}
 
 function main(
     ctx: CanvasRenderingContext2D,
@@ -17,199 +280,27 @@ function main(
     win?: Window,
     doc?: Document,
 ): void {
-    const gridWidth = 16;
-    const gridHeight = 16;
-    const gridSize = 50;
-    // const halfGridSize = gridSize / 2;
-    let path: number[];
+    const newGrid = new Grid(16, 16, 50);
+    const newPlayer = new Player(newGrid);
+    const newEnemy = new Enemy(newGrid, newPlayer);
+    const userInput = new UserInput(doc);
 
-    const player = {
-        gridx: 0,
-        gridy: 0,
-    };
-
-    const toPoint = {
-        gridx: Math.floor(randRange(0, gridWidth)),
-        gridy: Math.floor(randRange(0, gridHeight)),
-    };
-
-    function getPointId(point: Point) {
-        return point.y * gridWidth + point.x;
+    function update(): void {
+        newPlayer.update(userInput);
+        newEnemy.update();
+        userInput.clear();
     }
-
-    function getNeighborIds(currentId: number): number[] {
-        const currentX = currentId % gridWidth;
-        const currentY = Math.floor(currentId / gridWidth);
-
-        const neighborIds = [];
-
-        if (currentY - 1 >= 0) {
-            neighborIds.push(
-                getPointId({
-                    x: currentX,
-                    y: currentY - 1,
-                }),
-            );
-        }
-
-        if (currentX + 1 < gridWidth) {
-            neighborIds.push(
-                getPointId({
-                    x: currentX + 1,
-                    y: currentY,
-                }),
-            );
-        }
-
-        if (currentY + 1 < gridHeight) {
-            neighborIds.push(
-                getPointId({
-                    x: currentX,
-                    y: currentY + 1,
-                }),
-            );
-        }
-
-        if (currentX - 1 >= 0) {
-            neighborIds.push(
-                getPointId({
-                    x: currentX - 1,
-                    y: currentY,
-                }),
-            );
-        }
-
-        return neighborIds;
-    }
-
-    function heuristic(goal: number, current: number): number {
-        const srcX = goal % gridWidth;
-        const srcY = Math.floor(goal / gridWidth);
-
-        const targetX = current % gridWidth;
-        const targetY = Math.floor(goal / gridWidth);
-
-        const dx = targetX - srcX;
-        const dy = targetY - srcY;
-        return Math.sqrt(dx * dx + dy * dy);
-    }
-
-    function findPath(fromPoint: Point, toPoint: Point): number[] {
-        const frontier = new PQ((a: PointWithCost, b: PointWithCost) => b.cost - a.cost);
-        const fromPointId = getPointId(fromPoint);
-
-        const toPointId = getPointId(toPoint);
-        frontier.enq({ id: fromPointId, cost: 0 });
-        const comeFrom = {
-            [fromPointId]: -1,
-        };
-        const costSoFar = {
-            [fromPointId]: 0,
-        };
-
-        while (frontier.size() > 0) {
-            const current = frontier.deq();
-            if (current.id === toPointId) {
-                // found it
-                break;
-            }
-            const neighborIds = getNeighborIds(current.id);
-            for (let i = 0; i < neighborIds.length; i++) {
-                const nextNeighbor = neighborIds[i];
-                const newCost = costSoFar[current.id] + 1;
-                if (!costSoFar[nextNeighbor] || newCost < costSoFar[nextNeighbor]) {
-                    costSoFar[nextNeighbor] = newCost;
-                    const priority = newCost + heuristic(toPointId, nextNeighbor);
-                    frontier.enq({ id: nextNeighbor, cost: priority });
-                    comeFrom[nextNeighbor] = current.id;
-                }
-            }
-        }
-
-        const path = [];
-        let i = toPointId;
-        while (comeFrom[i] != fromPointId) {
-            path.push(comeFrom[i]);
-            i = comeFrom[i];
-        }
-
-        return path;
-    }
-
-    doc.addEventListener('mousemove', (ev) => {
-        player.gridx = Math.floor(ev.clientX / gridSize);
-        player.gridy = Math.floor(ev.clientY / gridSize);
-
-        path = findPath(
-            { x: player.gridx, y: player.gridy },
-            {
-                x: toPoint.gridx,
-                y: toPoint.gridy,
-            },
-        );
-
-        console.log('path:', path);
-    });
-
-    // function update(): void {}
 
     function render(): void {
         ctx.clearRect(0, 0, width, height);
-
-        // draw grid
-        ctx.save();
-        for (let i = 0; i < gridHeight; i++) {
-            const y = i * gridSize;
-            for (let j = 0; j < gridWidth; j++) {
-                const x = j * gridSize;
-                ctx.rect(x, y, gridSize, gridSize);
-                ctx.stroke();
-            }
-        }
-        ctx.restore();
-
-        console.log('player:', player);
-
-        // draw player
-        ctx.save();
-        ctx.fillStyle = 'rgb(200, 0, 0)';
-        ctx.fillRect(
-            player.gridx * gridSize,
-            player.gridy * gridSize,
-            gridSize,
-            gridSize,
-        );
-        ctx.restore();
-
-        // draw toPoint
-        ctx.save();
-        ctx.fillStyle = 'rgb(0, 200, 0)';
-        ctx.fillRect(
-            toPoint.gridx * gridSize,
-            toPoint.gridy * gridSize,
-            gridSize,
-            gridSize,
-        );
-        ctx.restore();
-
-        if (path && path.length > 0) {
-            ctx.save();
-            ctx.fillStyle = 'rgb(0, 0, 200)';
-            for (let i = 0; i < path.length; i++) {
-                const p = path[i];
-                const x = p % gridWidth;
-                const y = Math.floor(p / gridWidth);
-
-                ctx.fillRect(x * gridSize, y * gridSize, gridSize, gridSize);
-            }
-            ctx.restore();
-        }
+        newGrid.render(ctx);
+        newPlayer.render(ctx);
+        newEnemy.render(ctx);
     }
 
     function mainLoop(): void {
-        // update();
+        update();
         render();
-
         win.requestAnimationFrame(mainLoop);
     }
 
